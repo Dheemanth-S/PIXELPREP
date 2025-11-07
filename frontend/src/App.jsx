c2c839e5bd2158c2efae3bf8504f2b026532df15
Dheemanth-S <s38dheemanth@gmail.com>
2025-11-08 02:06:44 +0530

    Homepage Designing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,14 +11,24 @@ import { Navigate, Route, Routes } from "react-router";
 import ProblemsPage from "./Pages/ProblemsPage";
 import HomePage from "./Pages/HomePage";
 import { Toaster } from "react-hot-toast";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  if (!isLoaded) return null;
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/"
+          element={isSignedIn ? <Dashboard /> : <Navigate to={"/"} />}
+        />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
